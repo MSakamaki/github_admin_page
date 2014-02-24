e29f7abd48ce45b568a0b5a15dda0cc7126c0dd1
M.Sakamaki <daigouzigai5@gmail.com>
2014-02-24 19:51:54 +0900

    Updates

--- scripts/743c2eb6.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("githubAdminPageApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("githubAdminPageApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
